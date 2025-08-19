1ac136557e24c3c4142d0049a9405c34f3aecc5e
KYU <existence.m.p@gmail.com>
2025-08-20 01:30:27 +0900

    methodを役割ごとにファイルわけ

---
+++ py/getFileText.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from markitdown import MarkItDown
+from extractous import Extractor, TesseractOcrConfig
+import chardet
+
+# MicrosoftのMarkItDown版。
+def getFileText(filePath: str):
+	# 特定のファイル形式か判定
+	p = Path(filePath)
+	ext = p.suffix.lower()
+
+	match ext:
+		case ".txt" | ".md":
+			text = ""
+			# 文字コードの判定
+			with open(filePath, "rb") as file:
+				raw_data = file.read()
+				result = chardet.detect(raw_data)
+				encoding = result["encoding"]
+			# 判定した文字コードで開く
+			with open(filePath, "r", encoding=encoding) as file:
+				text = file.read()
+			return text
+		case ".docx" | ".xlsx" | ".pptx" | ".pdf":
+			md = MarkItDown(enable_plugins=False)
+			result = md.convert(filePath)
+			return result.text_content
+	return ""
+
+# OCRにはtesseract-ocrが必要。
+def getFileTextEx(filePath: str):
+	# 特定のファイル形式か判定
+	p = Path(filePath)
+	ext = p.suffix.lower()
+	match ext:
+		case ".png" | ".jpeg" | ".tiff" | ".bmp" | ".gif" | ".svg":
+			# `sudo apt install tesseract-ocr tesseract-ocr-jpn`
+			extractor = Extractor().set_ocr_config(TesseractOcrConfig().set_language("jpn"))
+			result, metadata = extractor.extract_file_to_string(filePath)
+			return result
+		case ".docx" | ".doc" | ".xlsx" | ".xls" | ".pptx" | ".ppt" | ".pdf" | ".tsv" | ".csv" | ".html" | ".txt" | ".md":
+			extractor = Extractor()
+			result, metadata = extractor.extract_file_to_string(filePath)
+			return result
+	return ""
