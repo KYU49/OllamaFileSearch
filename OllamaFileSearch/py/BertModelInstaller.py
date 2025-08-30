e7df92dfb1db0d9cb30dcec3885dbc7633b75b32
KYU <existence.m.p@gmail.com>
2025-08-30 16:55:55 +0000

    AutoModelのダウンロードも追記

--- OllamaFileSearch/py/BertModelInstaller.py
+++ OllamaFileSearch/py/BertModelInstaller.py
@@ -1,9 +1,14 @@
 import os
 import torch
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
 
 def main():
 	AutoTokenizer.from_pretrained(
+		"sbintuitions/modernbert-ja-310m", 
+		torch_dtype=torch.float16, 
+		cache_dir=os.path.dirname(os.path.abspath(__file__)) + "/.cache"
+	)
+	AutoModel.from_pretrained(
 		"sbintuitions/modernbert-ja-310m", 
 		torch_dtype=torch.float16, 
 		cache_dir=os.path.dirname(os.path.abspath(__file__)) + "/.cache"
